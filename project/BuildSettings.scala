bffd9b08c3fe9c7198f56d6a2c5fec06a7db8880
Thanh Le <lenguyenthanh@hotmail.com>
2026-06-27 13:43:20 +0200

    Remove unused plugins

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -2,9 +2,9 @@
  * Copyright (C) from 2022 The Play Framework Contributors <https://github.com/playframework>, 2011-2021 Lightbend Inc. <https://www.lightbend.com>
  */
 import java.util.regex.Pattern
-import interplay._
-import interplay.PlayBuildBase.autoImport._
-import interplay.ScalaVersions._
+// import interplay._
+// import interplay.PlayBuildBase.autoImport._
+// import interplay.ScalaVersions._
 import sbt._
 import sbt.Keys._
 import sbt.ScriptedPlugin.autoImport._
@@ -21,8 +21,6 @@ object BuildSettings {
   /** These settings are used by all projects. */
   def playCommonSettings: Seq[Setting[_]] = Def.settings(
     scalaVersion    := "3.7.0",
-    ivyLoggingLevel := UpdateLogging.DownloadOnly,
-    ivyConfigurations ++= Seq(SourcesApplication),
     javacOptions ++= Seq("-encoding", "UTF-8", "-Xlint:unchecked", "-Xlint:deprecation", "--release", "21"),
     (Compile / scalacOptions)       := Seq("-release:21"),
     (Compile / doc / scalacOptions) := Seq("-no-java-comments"),
@@ -37,27 +35,10 @@ object BuildSettings {
     version := playVersion
   )
 
-  /** A project that is shared between the sbt runtime and the Play runtime. */
-  def PlayNonCrossBuiltProject(name: String, dir: String): Project = {
-    Project(name, file(dir))
-      .enablePlugins(PlaySbtLibrary)
-      .settings(playCommonSettings)
-      .settings(
-        autoScalaLibrary := false,
-        crossPaths       := false,
-      )
-  }
-
   /** A project that is in the Play runtime. */
   def PlayCrossBuiltProject(name: String, dir: String): Project = {
     Project(name, file(dir))
-      .enablePlugins(PlayLibrary)
-      .settings(playCommonSettings)
+    .settings(playCommonSettings)
   }
-
-  def disablePublishing = Def.settings(
-    (publish / skip) := true,
-    publishLocal     := {},
-  )
 
 }
