128f695a956751eb1e7a1bd89d65734af5649629
Thibault Duplessis <t@lichess.org>
2026-07-25 20:08:41 +0200

    Revert "Make GeneratedRouter.named return EssentialAction"
    This reverts commit c64e8f864f405625377f180832eee75cee35ef77.

--- core/play/src/main/scala/play/core/routing/GeneratedRouter.scala
+++ core/play/src/main/scala/play/core/routing/GeneratedRouter.scala
@@ -76,9 +76,8 @@ abstract class GeneratedRouter extends Router {
     errorHandler.onClientError(request, play.api.http.Status.BAD_REQUEST, error)
   }
 
-  def named(name: String)(generator: => EssentialAction): EssentialAction =
-    EssentialAction: (request: RequestHeader) =>
-      generator(request.addAttr(play.api.routing.Router.Attrs.ActionName, name))
+  def named(name: String)(generator: => EssentialAction) =
+    (request: RequestHeader) => (request.addAttr(play.api.routing.Router.Attrs.ActionName, name), generator)
 
   def call(generator: => EssentialAction): EssentialAction =
     generator
